944374b5141205f38b7f6be99f85c5949e9237bd
s0a9 <sarusaruhasan82@gmail.com>
2024-07-04 00:11:05 +0530

    setting up initial standard structure and creating route for different end points

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
-require("dotenv").config();
+// routes
+const userRoute = require("./routes/userRoutes");
+const commentRoute = require("./routes/commentRouters");
+const blogRoute = require("./routes/blogRouter");
 
-const PORT = process.env.PORT || 4000
+app.use(express.json());
+
+app.use("/api/user", userRoute);
+app.use("api/comments", commentRoute);
+app.use("/api/blogs", blogRoute);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log("Welcome to my blog app...");
-})
-
+  console.log("Welcome to my blog app...");
+});
