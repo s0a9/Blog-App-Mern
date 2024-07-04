2bbaf8fb7ef4d111cebb7958afe17f3e7118c6c8
s0a9 <sarusaruhasan82@gmail.com>
2024-07-04 20:44:54 +0530

    user auth with mongodb NOT JWT THOUGH YET TO DO

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,34 @@ const User = require("../models/userModel");
 
 // login
 // method: post
-const login = (req, res) => {
-  res.json({ mssg: "Login User" });
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ mssg: "All field's are mandatory" });
+  }
+
+  try {
+    const user = await User.login(email, password);
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // signup
 // method: post
-const signup = (req, res) => {
-  res.json({ mssg: "Signup User" });
+const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const user = await User.signup(username, email, password);
+    res.status(200).json({ mssg: "Signup User", user });
+    if (!user) {
+      throw new Error({ message: "can't create user" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcyrpt = require("bcrypt");
+
 const userSchema = mongoose.Schema(
   {
     username: {
@@ -18,5 +20,39 @@ const userSchema = mongoose.Schema(
     timestamp: true,
   }
 );
+
+userSchema.statics.signup = async function (username, email, password) {
+  if (!username || !password || !email) {
+    throw new Error("All fields are importent.");
+  }
+
+  const salt = await bcyrpt.genSalt(10);
+  const hashedPassword = await bcyrpt.hash(password, salt);
+
+  const isUserIn = await this.findOne({ email });
+
+  if (isUserIn) {
+    throw new Error("User already registed");
+  }
+
+  const user = await this.create({ username, password: hashedPassword, email });
+  return user;
+};
+
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("User not found");
+  }
+
+  const isAuth = await bcyrpt.compare(password, user.password);
+
+  if (!isAuth) {
+    throw Error("Provide apporitate password");
+  }
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
